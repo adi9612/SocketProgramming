bdd93e943a6dde85d2d39b7429c16c56dccbe4d6
adi9612 <43066525+adi9612@users.noreply.github.com>
2020-07-29 13:30:35 +0530

    Add files via upload

---
+++ SocketProgramming/src/com/company/MultiThreadedServer.java
@@ -0,0 +1,24 @@
+package com.company;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class MultiThreadedServer {
+
+    public static void main(String[] args) {
+        try {
+            ServerSocket socket = new ServerSocket(4001);
+            //without while statement server will go down
+            while(true) {
+                Socket clientSocket = socket.accept();
+
+                Messaging obj = new Messaging(clientSocket);
+                obj.start();
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
